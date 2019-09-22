a9b27c854d373ba71c066815c967d4560677fcfc
Rishabh Rai <rishabh.rai@utexas.edu>
2019-09-22 05:53:08 -0500

    working, bad performance, needs hyperparameter tuning

--- td3/td3.py
+++ td3/td3.py
@@ -14,7 +14,7 @@ import torch
 
 import gym
 
-model_name = "TD3-Pendulum-v0"
+model_name = "TD3-LunarLanderContinuous-v2"
 
 num_iterations = 10000
 replay_memory_max_size = 1000
@@ -27,6 +27,8 @@ epsilon_limit = 0.5
 min_action = -2
 max_action = 2
 
+writer = SummaryWriter()
+
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 # TODO convert to gpu
@@ -35,44 +37,33 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 class TD3CartpoleActorNN(nn.Module):
     def __init__(self):
         super(TD3CartpoleActorNN, self).__init__()
-        self.fc1 = nn.Linear(3, 10)
-        self.fc2 = nn.Linear(10, 10)
-        self.fc3 = nn.Linear(10, 1)
+        self.fc1 = nn.Linear(8, 20)
+        self.fc2 = nn.Linear(20, 20)
+        self.fc3 = nn.Linear(20, 2)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = F.tanh(self.fc3(x))
+        x = torch.tanh(self.fc3(x))
         return x
 
-    # def load_state_dict(self, state_dict, strict=True):
-    #     super(TD3CartpoleActorNN, self).load_state_dict(state_dict)
-    #
-    # def state_dict(self, destination=None, prefix='', keep_vars=False):
-    #     return super(TD3CartpoleActorNN, self).state_dict()
 
 # define critic network
 class TD3CartpoleCriticNN(nn.Module):
     def __init__(self):
         super(TD3CartpoleCriticNN, self).__init__()
-        self.fc1 = nn.Linear(4, 10)
-        self.fc2 = nn.Linear(10, 10)
-        self.fc3 = nn.Linear(10, 1)
+        self.fc1 = nn.Linear(10, 20)
+        self.fc2 = nn.Linear(20, 20)
+        self.fc3 = nn.Linear(20, 1)
 
     def forward(self, state, action):
-        print("critic forward shape", state.size(), action.size())
-        x = cat((state, action), dim=0)  # concatenate inputs along 0th dimension
-        print("critic x shape", x.size())
+        # print("critic forward shape", state.size(), action.size())
+        x = cat((state, action), dim=1)  # concatenate inputs along 0th dimension
+        # print("critic x shape", x.size())
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
-
-    # def load_state_dict(self, state_dict, strict=True):
-    #     super(TD3CartpoleCriticNN, self).load_state_dict(state_dict)
-    #
-    # def state_dict(self, destination=None, prefix='', keep_vars=False):
-    #     return super(TD3CartpoleCriticNN, self).state_dict()
 
 
 # Initialize critic networks Qθ1, Qθ2, and actor network πφ with random parameters θ1, θ2, φ
@@ -88,31 +79,26 @@ critic_target_net_2.load_state_dict(critic_net_2.state_dict())
 actor_target_net = TD3CartpoleActorNN()
 actor_target_net.load_state_dict(actor_net.state_dict())
 
-# # convert to float
-# critic_net_1 = critic_net_1.float()
-# critic_target_net_1 = critic_target_net_1.float()
-# critic_net_2 = critic_net_2.float()
-# critic_target_net_2 = critic_target_net_2.float()
-# actor_net = actor_net.float()
-# actor_target_net = actor_target_net.float()
-
 # Initialize replay buffer B
 replay_buffer = []
 
-# initialize the environment
-env = gym.make('Pendulum-v0')
+# initialize the environments
+env = gym.make('LunarLanderContinuous-v2')
 curr_state = env.reset()
+curr_state = tensor(curr_state).float()
+
+test_env = gym.make('LunarLanderContinuous-v2')
+curr_test_state = test_env.reset()
 
 # initialize critic losses
-critic_net_1_loss = nn.MSELoss(reduction='mean')
-critic_net_2_loss = nn.MSELoss(reduction='mean')
+critic_net_1_loss_fn = nn.MSELoss(reduction='mean')
+critic_net_2_loss_fn = nn.MSELoss(reduction='mean')
 
 # initialize optimizers
 critic_net_1_optimizer = optim.Adam(critic_net_1.parameters(), lr=0.001)
 critic_net_2_optimizer = optim.Adam(critic_net_2.parameters(), lr=0.001)
 actor_net_optimizer = optim.Adam(actor_net.parameters(), lr=0.001)
 
-# TODO replace 3 and 4 with -1?
 # TODO remove empty comma?
 
 # initialize normal distribution N
@@ -121,61 +107,49 @@ normal_dist = normal.Normal(0, sigma)
 # for t = 1 to T do
 for t in range(num_iterations):
     # Select action with exploration noise a ∼ πφ(s) + ϵ ,ϵ ∼ N (0, σ), and observe reward r and new state s'
-    # action = actor_net(tensor(curr_state).view(1, 3,)) + np.random.normal(0, sigma, 1)  # TODO clip & sigma
-    action = clamp(actor_net(tensor(curr_state).view(1, 3,).float()) + clamp(normal_dist.sample(), -epsilon_limit, epsilon_limit), min_action, max_action)
-    action_no_grad = action.detach()
-    print("action size", action.size())
-    next_state, reward, done, _ = env.step(action_no_grad)
+    action = clamp(actor_net(curr_state.view(1, -1,).float()) + clamp(normal_dist.sample(), -epsilon_limit, epsilon_limit), min_action, max_action)
+    action = action.detach().numpy().squeeze()
+    next_state, reward, done, _ = env.step(action)
 
     # Store transition tuple (s, a, r, s') in B
-    replay_buffer.append((tensor(curr_state).view(3,), tensor(action).view(1,), tensor(reward).view(1,), tensor(next_state).view(3,), tensor(done).view(1,)))
+    replay_buffer.append((curr_state.view(1, -1,), tensor(action).view(1, -1,), tensor(reward).float().view(1, 1,), tensor(next_state).view(1, -1,), tensor(done).view(1, 1,)))
     if len(replay_buffer) > replay_memory_max_size + 10:
         replay_buffer = replay_buffer[10:]
 
     # Sample mini-batch of N transitions (s, a, r, s') from B
     transitions_minibatch = random.choices(replay_buffer, k=minibatch_size)
-    minibatch_states, minibatch_actions, minibatch_rewards, minibatch_next_states, minibatch_dones = zip(*transitions_minibatch)
+    minibatch_states, minibatch_actions, minibatch_rewards, minibatch_next_states, minibatch_dones = [cat(mb, dim=0) for mb in zip(*transitions_minibatch)]
+    minibatch_states = minibatch_states.float()
 
     # a˜ ← πφ0 (s') + ϵ, ϵ ∼ clip(N (0, σ˜), −c, c)
-    # next_actions = [actor_target_net(np.asarray(s_prime)).reshape(1, 3,) + np.random.normal(0, sigma, 1)
-    #                 for _, _, _, s_prime in transitions_minibatch]  # TODO clip & sigma
-    minibatch_next_actions = clamp(actor_target_net(tensor(minibatch_next_states)) + clamp(normal_dist.sample(sample_shape=(minibatch_size, 1,)), -epsilon_limit, epsilon_limit), min_action, max_action)
-    print("minibatch next actions size", minibatch_next_actions.size())
+    sampled = normal_dist.sample(sample_shape=(minibatch_size, 1,))
+    minibatch_next_actions = clamp(actor_target_net(minibatch_next_states) + clamp(sampled, -epsilon_limit, epsilon_limit), min_action, max_action)
 
-    # TODO y should be r depending on terminal state
     # y ← r + γ mini=1,2 Qθ'i(s', a˜)
-    minibatch_y = minibatch_rewards + discount_rate * torch.min(critic_target_net_1(minibatch_next_states, minibatch_next_actions), critic_target_net_2(minibatch_next_states, minibatch_next_actions)) * tensor(-minibatch_dones + 1)
-    print("minibatch y size", minibatch_y.size())
-
-    minibatch_x = tensor(minibatch_states, minibatch_actions)
-
-    # transitions_minibatch_y = []
-    # for (_, _, r, s_prime), a_prime in zip(transitions_minibatch, next_actions):
-    #     critic_x = np.asarray(s_prime + a_prime).reshape(1, 4,) # TODO appending working?
-    #     a_prime_value = (min(critic_target_net_1(critic_x), critic_target_net_2(critic_x)))
-    #     transitions_minibatch_y.append(r + discount_rate*a_prime_value)
-
-    # transitions_minibatch_y = np.asarray(transitions_minibatch_y).reshape(minibatch_size, 1,)
-    # transitions_minibatch_x = np.asarray([np.asarray(s + a).reshape(1, 4,) for s, a, r, s_prime in transitions_minibatch]).reshape((minibatch_size, 1,))
+    minibatch_y = minibatch_rewards + discount_rate * torch.min(critic_target_net_1(minibatch_next_states, minibatch_next_actions), critic_target_net_2(minibatch_next_states, minibatch_next_actions)) * (-minibatch_dones.float() + 1)
 
     # Update critics θi ← argminθi (N^−1)*Σ(y−Qθi(s, a))^2
     critic_net_1.zero_grad()
-    critic_net_1_loss_output = critic_net_1_loss(minibatch_x, minibatch_y)
-    critic_net_1_loss.backward()
+    critic_net_1_loss = critic_net_1_loss_fn(critic_net_1(minibatch_states, minibatch_actions), minibatch_y)
+    critic_net_1_loss.backward(retain_graph=True)
     critic_net_1_optimizer.step()
+    writer.add_scalar('Loss/critic_net_1', critic_net_1_loss.detach().numpy().squeeze(), t)
 
     critic_net_2.zero_grad()
-    critic_net_2_loss_output = critic_net_2_loss(minibatch_x, minibatch_y)
-    critic_net_2_loss.backward()
+    critic_net_2_loss = critic_net_2_loss_fn(critic_net_2(minibatch_states, minibatch_actions), minibatch_y)
+    # print(critic_net_2_loss.grad_fn)
+    critic_net_2_loss.backward(retain_graph=True)  # TODO correct?
     critic_net_2_optimizer.step()
+    writer.add_scalar('Loss/critic_net_2', critic_net_2_loss.detach().numpy().squeeze(), t)
 
     # if t mod d then
     if t % steps_until_policy_update == 0:
         # Update φ by the deterministic policy gradient: ∇φJ(φ) = N −1 P∇aQθ1(s, a)|a=πφ(s)∇φπφ(s)
         actor_net.zero_grad()
-        actor_net_loss = -1 * critic_net_1(minibatch_states, actor_net(minibatch_states)).mean()
+        actor_net_loss = -1 * critic_net_1(minibatch_states, actor_net(minibatch_states)).mean()  # gradient ascent
         actor_net_loss.backward()
         actor_net_optimizer.step()
+        writer.add_scalar('Loss/actor_net', actor_net_loss.detach().numpy().squeeze(), t)
 
         # Update target networks:
         # θ'i ← τθi + (1 − τ )θ'i
@@ -192,15 +166,62 @@ for t in range(num_iterations):
     # end if
     if t % (num_iterations // 10) == 0 or t == num_iterations - 1:
         print("iter", t)
-        torch.save(critic_net_1.state_dict(), 'td3/models/' + model_name + '-critic_net_1')
-        torch.save(critic_target_net_1.state_dict(), 'td3/models/' + model_name + '-critic_target_net_1')
-        torch.save(critic_net_2.state_dict(), 'td3/models/' + model_name + '-critic_net_2')
-        torch.save(critic_target_net_2.state_dict(), 'td3/models/' + model_name + '-critic_target_net_2')
-        torch.save(actor_net.state_dict(), 'td3/models/' + model_name + '-actor_net')
-        torch.save(actor_target_net.state_dict(), 'td3/models/' + model_name + '-actor_target_net')
+        torch.save(critic_net_1.state_dict(), 'models/' + model_name + '-critic_net_1.pkl')
+        torch.save(critic_target_net_1.state_dict(), 'models/' + model_name + '-critic_target_net_1.pkl')
+        torch.save(critic_net_2.state_dict(), 'models/' + model_name + '-critic_net_2.pkl')
+        torch.save(critic_target_net_2.state_dict(), 'models/' + model_name + '-critic_target_net_2.pkl')
+        torch.save(actor_net.state_dict(), 'models/' + model_name + '-actor_net.pkl')
+        torch.save(actor_target_net.state_dict(), 'models/' + model_name + '-actor_target_net.pkl')
 
     if not done:
-        curr_state = next_state
+        curr_state = tensor(next_state).float()
     else:
         curr_state = env.reset()
+        curr_state = tensor(curr_state).float()
+
+    if t % (num_iterations // 25) == 0 or t == num_iterations - 1:
+        render = False
+        num_eval_episodes = 10
+
+        test_obs = test_env.reset()
+        episode_rewards = []
+        episode_reward = 0
+        while len(episode_rewards) < num_eval_episodes:
+            test_action = actor_net(tensor(test_obs).float()).detach().numpy().squeeze()
+            test_obs, test_reward, test_done, _ = test_env.step(test_action)
+            episode_reward += test_reward
+            if test_done:
+                episode_rewards.append(episode_reward)
+                episode_reward = 0
+                test_obs = test_env.reset()
+            if render:
+                test_env.render()
+
+        avg_episode_rewards = np.mean(np.asarray(episode_rewards))
+        writer.add_scalar('Reward/test', avg_episode_rewards, t)
 # end for
+
+render = True
+num_eval_episodes = 10
+
+obs = env.reset()
+episode_rewards = []
+episode_reward = 0
+while len(episode_rewards) < num_eval_episodes:
+    action = actor_net(tensor(obs).float()).detach().numpy().squeeze()
+    obs, reward, done, _ = env.step(action)
+    episode_reward += reward
+    if done:
+        episode_rewards.append(episode_reward)
+        episode_reward = 0
+        obs = env.reset()
+    if render:
+        env.render()
+
+episode_rewards = np.asarray(episode_rewards)
+episode_length_histogram = plt.hist(episode_rewards)
+plt.title("Episode Rewards")
+plt.xlabel("Total Reward")
+plt.ylabel("Frequency")
+plt.savefig("episode_rewards_hist.png")
+print("Mean total episode reward:", np.mean(episode_rewards))
